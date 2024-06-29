522e7e2e786484f35f285517923011c7941fb79e
berta <antonelaluciabertarini@gmail.com>
2024-06-29 16:43:03 -0700

    root folders update - server set up

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+app.get("/index", (req, res) =>{
+    res.send('Hola Mundo');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening`+`at http://localhost:${port}`);
+});
+
+app.use(express.static('public'));
+
---
+++ src/app.js
@@ -0,0 +1,16 @@
+/* const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get("/index", (req, res) =>{
+    res.send('Hola Mundo');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening`+`at http://localhost:${port}`);
+});
+
+application.use(express.static('public'));
+
+*/
